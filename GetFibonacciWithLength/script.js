f7e72335dd0ada0f304c035f0dee276e84fb3829
Nazar <nazarvelicko@gmail.com>
2023-03-28 18:41:24 +0300

    Done isuues #2, #3, #4, #5, #6

---
+++ GetFibonacciWithLength/script.js
@@ -0,0 +1,13 @@
+function generateFibonacciTimes(num) {
+  let a = 0,
+  b = 1,
+      c,
+      str = "";
+  for (let i = -1; i < num - 1; i++) {
+    c = a + b;
+    a = b;
+    b = c;
+    str += a + ', ';
+  }
+  return str;
+}
---
+++ fibonacciWithBiggestNumber/script.js
@@ -0,0 +1,18 @@
+
+function generateFibonacciLenght(num) {
+  let a = 0,
+      b = 1,
+      c,
+      str = [];
+  for (let i = 0; i <= num; i++) {
+    c = a + b;
+    a = b;
+    b = c;
+    if (a > num ) {
+      break;
+    } else {
+        str += a + ", ";
+    }
+  }
+  return str;
+}
